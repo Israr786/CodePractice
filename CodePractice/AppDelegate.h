5b42b031549f19475aebae107579bd286d7ab8a1
Israr786 <israrul786@gmail.com>
2018-03-26 14:48:47 -0700

    Initial Commit

---
+++ CodePractice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CodePractice
+//
+//  Created by Apple on 3/26/18.
+//  Copyright © 2018 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CodePractice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CodePractice
+//
+//  Created by Apple on 3/26/18.
+//  Copyright © 2018 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
